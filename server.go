7da6353618f4e2ce5c57c9f1013d15a24d6d9a3b
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2021-11-01 18:39:47 +0100

    Fix cors

--- server.go
+++ server.go
@@ -3,28 +3,33 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
-	"fmt"
 )
 
-//type Todo struct {
-//	title       string `json:"title"`
-//	description string `json:"description"`
-//	completed   bool   `json:"completed"`
-//	height      int    `json:"height"`
-//}
 type Todo struct {
-	name string `json:"name"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+	Height      int    `json:"height"`
 }
+//type Todo struct {
+//	Name string `json:"name"`
+//}
 
 var db = make(map[string]Todo)
 
 func main() {
 	r := gin.Default()
 
+	//r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
+	}))
+
 	r.GET("/todos/:name", func(c *gin.Context) {
 		user := c.Params.ByName("name")
 		todo, ok := db[user]
-		fmt.Println(todo)
 		if ok {
 			c.JSON(200, gin.H{"user": user, "todo": todo})
 		} else {
@@ -41,16 +46,7 @@ func main() {
 			db[user] = todo
 			c.JSON(200, gin.H{"status": "ok"})
 		}
-
-		return
 	})
-
-	//r.Use(cors.Default())
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST"},
-		AllowHeaders:     []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
-	}))
 
 	r.Run()
 }
---
+++ test.sh
@@ -0,0 +1,4 @@
+curl --header "Content-Type: application/json" \
+	--request POST \
+	--data '{"title": "Learn EE", "description": "use rust", "completed": false, "height": 50}' \
+	http://localhost:8080/add/michal
