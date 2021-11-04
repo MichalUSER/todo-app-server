1da0c5ca6e295994e88fffce3e79c7ac942aac92
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2021-11-04 20:34:06 +0100

    accept array of todos

--- server.go
+++ server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 type Todo struct {
@@ -11,27 +11,24 @@ type Todo struct {
 	Completed   bool   `json:"completed"`
 	Height      int    `json:"height"`
 }
-//type Todo struct {
-//	Name string `json:"name"`
-//}
 
-var db = make(map[string]Todo)
+var db = make(map[string][]Todo)
 
 func main() {
 	r := gin.Default()
 
 	//r.Use(cors.Default())
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST"},
-		AllowHeaders:     []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
 	}))
 
 	r.GET("/todos/:name", func(c *gin.Context) {
 		user := c.Params.ByName("name")
 		todo, ok := db[user]
 		if ok {
-			c.JSON(200, gin.H{"user": user, "todo": todo})
+			c.JSON(200, gin.H{"user": user, "todos": todo})
 		} else {
 			c.JSON(200, gin.H{"user": user, "status": "no value"})
 		}
@@ -39,11 +36,11 @@ func main() {
 
 	r.POST("/add/:name", func(c *gin.Context) {
 		user := c.Params.ByName("name")
-		var todo Todo
-		if err := c.BindJSON(&todo); err != nil {
+		var todos []Todo
+		if err := c.BindJSON(&todos); err != nil {
 			c.JSON(200, gin.H{"status": err.Error()})
 		} else {
-			db[user] = todo
+			db[user] = todos
 			c.JSON(200, gin.H{"status": "ok"})
 		}
 	})
