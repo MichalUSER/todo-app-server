51f0f5c7186b288cbd2ba91806952dea46f78a6b
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2021-11-04 20:33:27 +0100

    improve test

--- test.sh
+++ test.sh
@@ -2,3 +2,5 @@ curl --header "Content-Type: application/json" \
 	--request POST \
 	--data '{"title": "Learn EE", "description": "use rust", "completed": false, "height": 50}' \
 	http://localhost:8080/add/michal
+
+curl http://localhost:8080/todos/michal
